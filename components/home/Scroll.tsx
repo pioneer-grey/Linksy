70166ea62dd3ea8c1f7a304300ef85ea8df8473e
pioneer-grey <xzozandersubhan@gmail.com>
2026-04-22 00:52:31 +0500

    Scroll Heading

--- components/home/Scroll.tsx
+++ components/home/Scroll.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { motion } from "framer-motion";
 import img_1 from "@/public/link-bio-2.png" 
 import img_2 from "@/public/link-in-bio-3.png" 
 import img_6 from "@/public/link-bio-7.png" 
@@ -7,7 +8,20 @@ const Scroll = () => {
   return (
     <section className="relative w-full overflow-hidden  px-4 py-16 md:py-24 lg:py-32">
 
+
       <div className="mx-auto max-w-7xl">
+             <motion.div
+  initial={{ opacity: 0, y: -20 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.6 }}
+  className="mb-12 text-center md:mb-16"
+>
+  <h2 className="mb-4 text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl">
+    Turn followers into customers
+  </h2>
+  <p className="mx-auto max-w-2xl text-base text-muted-foreground md:text-lg">
+Linksy’s link in bio tool helps you navigate visitors directly to your social media channels, products, blogs, music, and other destinations that drive your business forward.   </p>
+</motion.div>
         <div className="grid gap-8 lg:grid-cols-2 lg:gap-12">
         {/* Feature 1 */}
   <div className='flex justify-center items-center'>
@@ -24,7 +38,7 @@ const Scroll = () => {
   </div>
 
   {/* Feature 2 */}
-  <div className='flex flex-col items-center justify-center'>
+  <div className='hidden md:flex flex-col items-center justify-center'>
     <h1 className='bg-clip-text text-3xl font-bold tracking-tight md:mb-4 md:text-4xl lg:text-5xl'>
       Track Clicks & Performance
     </h1>
@@ -36,6 +50,16 @@ const Scroll = () => {
   <div className='flex justify-center items-center'>
     <Image src={img_2} alt='Analytics dashboard preview' />
   </div>
+
+    <div className='flex md:hidden flex-col items-center justify-center'>
+    <h1 className='bg-clip-text text-3xl font-bold tracking-tight md:mb-4 md:text-4xl lg:text-5xl'>
+      Track Clicks & Performance
+    </h1>
+    <p className='text-muted-foreground text-base md:text-lg'>
+      Get real-time insights into your audience. See what’s working and optimize your links for better engagement.
+    </p>
+  </div>
+
 
   {/* Feature 3 */}
   <div className='flex justify-center items-center'>
