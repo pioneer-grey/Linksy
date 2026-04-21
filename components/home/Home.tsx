a0b0efec1856d51dc92ac8bceeb1e62ed7efcf63
pioneer-grey <xzozandersubhan@gmail.com>
2026-04-22 00:45:32 +0500

    Footer

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {Hero} from './Hero'
 import Navbar from "./Navbar"
 import Scroll from "./Scroll"
+import {Features} from "./Features"
+import {Footer} from "./Footer"
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
     <Scroll/>
+    <Features/>
+    <Footer/>
     </>
   )
 }
